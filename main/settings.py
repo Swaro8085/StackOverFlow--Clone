ce56d6827bcaa6cd368f9c76f758e06391b77125
deva0510 <155135755+deva0510@users.noreply.github.com>
2024-04-20 12:37:41 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -16,7 +16,7 @@ settings.configure()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://stackoverflow---clone.herokuapp.com/','localhost','http://127.0.0.1','stackoverflow---clone.herokuapp.com','.herokuapp.com']
+ALLOWED_HOSTS = ['*','https://stackoverflow---clone.herokuapp.com/','localhost','http://127.0.0.1','stackoverflow---clone.herokuapp.com','.herokuapp.com']
 
 # Application definition
 
